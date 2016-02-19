ca6fa0a37ce37fa2dd94a44b6fe8883f1e3fb08f
citi <citi@ug4c07.windows.cin.ufpe.br>
2016-02-19 12:04:53 -0300

    103

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,9 @@
 class BooksController < ApplicationController
 
+def new
+	@book = Book.new
+end
+
 def index
 	@books = Book.all
 end
@@ -13,6 +17,21 @@ end
 
 def show
 	@book = Book.find(params[:id])
+end
+
+def edit
+	@book = Book.find(params[:id])
+end
+
+def update
+	@book = Book.find(params[:id])
+
+	if @book.update(book_params)
+		redirect_to @book
+	else
+		render 'edit'
+	end
+
 end
 
 private
